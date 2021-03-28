a9bec59eface31d7c5f824544a1f7307331544b4
Rafael Costa <rafaelsevla@gmail.com>
2021-03-28 17:52:10 -0300

    Add routes to menu items

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,6 +1,6 @@
-import React, { Suspense } from 'react'
+import React, { lazy, Suspense } from 'react'
 import styled from 'styled-components'
-import { Route, Switch } from 'react-router-dom'
+import { Link, Route, Switch, useLocation } from 'react-router-dom'
 import {
   Divider,
   Drawer as MaterialDrawer,
@@ -10,53 +10,84 @@ import {
   Typography
 } from '@material-ui/core'
 
-const Main = () => (
-  <>
-    <Drawer variant='permanent'>
-      <DrawerContent>
-        <Typography variant='h4'>
-          React-zzaria
-        </Typography>
+import * as routes from 'routes'
 
-        <Typography>
-          (Sistema de cadastro)
-        </Typography>
-      </DrawerContent>
+const Orders = lazy(() => import('pages/orders'))
+const PizzasSizes = lazy(() => import('pages/pizzas-sizes'))
+const PizzasFlavours = lazy(() => import('pages/pizzas-flavours'))
 
-      <Divider />
+const Main = () => {
+  const { pathname } = useLocation()
 
-      <List>
-        {menuItens.map(item => (
-          <ListItem key={item.label} button>
-            <ListItemText>
-              {item.label}
-            </ListItemText>
-          </ListItem>
-        ))}
-      </List>
-    </Drawer>
+  return (
+    <>
+      <Drawer variant='permanent'>
+        <DrawerContent>
+          <Typography variant='h4'>
+            React-zzaria
+          </Typography>
 
-    <Content>
-      <Suspense fallback='loading...'>
-        <Switch>
-          <Route>
-            <h1>main</h1>
-          </Route>
-        </Switch>
-      </Suspense>
-    </Content>
-  </>
-)
+          <Typography>
+            (Sistema de cadastro)
+          </Typography>
+        </DrawerContent>
+
+        <Divider />
+
+        <List>
+          {menuItens.map(item => (
+            <ListItem
+              key={item.label}
+              button
+              selected={pathname === item.link}
+              component={Link}
+              to={item.link}
+            >
+              <ListItemText>
+                {item.label}
+              </ListItemText>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+
+      <Content>
+        <Suspense fallback='loading...'>
+          <Switch>
+            {menuItens.map(item => (
+              <Route
+                key={item.link}
+                path={item.link}
+                exact={item.exact}
+              >
+                <item.component />
+              </Route>
+            ))}
+          </Switch>
+        </Suspense>
+      </Content>
+    </>
+  )
+}
 
 const menuItens = [
   {
-    label: 'Pedidos'
+    label: 'Pedidos',
+    link: routes.HOME,
+    component: Orders,
+    exact: true
   },
   {
-    label: 'Tamanhos'
+    label: 'Tamanhos',
+    link: routes.PIZZAS_SIZES,
+    component: PizzasSizes,
+    exact: false
   },
   {
-    label: 'Sabores'
+    label: 'Sabores',
+    link: routes.PIZZAS_FLAVOURS,
+    component: PizzasFlavours,
+    exact: false
   }
 ]
 
---
+++ src/pages/pizzas-flavours/pizzas-flavours.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function PizzasFlavours () {
+  return <h1>Sabores</h1> 
+}
+
+export default PizzasFlavours
